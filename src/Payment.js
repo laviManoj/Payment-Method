598f038d4163a10e77b0c8198763752bdc3b6402
laviManoj <lavigeremanoj@gmail.com>
2023-08-02 17:50:02 +0530

    Added styles payment method and images adding

--- src/Payment.js
+++ src/Payment.js
@@ -1,19 +1,22 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Payment.css";
 import axios from "axios";
+import Creditcard from "./assets/images/Creditcard.png"
+import Cvv from "./assets/images/cvv.png"
+import expiredDate from "./assets/images/expiredDate.png"
+
 
 const PaymentPage = () => {
-
-
-const [banking, setBanking] = useState([]);
-
+  const [banking, setBanking] = useState([]);
+  const [error, setError] = useState([]);
   const [selectedPaymentMethod, setSelectedPaymentMethod] =
     useState("bankAccount");
+
   const [bankAccount, setBankAccount] = useState({
     ifscCode: "",
     accountHolderName: "",
     accountNumber: "",
-    banks:"",
+    banks: "",
   });
 
   const [upi, setUpi] = useState("");
@@ -40,9 +43,9 @@ const [banking, setBanking] = useState([]);
     const { name, value } = event.target;
     setCreditCard({ ...creditCard, [name]: value });
   };
-  const handleChangebanks =( event) =>{
+  const handleChangebanks = (event) => {
     setBanking(event.target.value);
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -55,49 +58,45 @@ const [banking, setBanking] = useState([]);
     });
   };
 
+  //  useEffect (() =>{
 
-//  useEffect (() =>{
+  //   const headers = {
+  //     Authorization: 'Basic cnpwX3Rlc3RfU1p2a3hIOGlGT01mSmI6'
+  //   }
+  //   fetch("https://api.razorpay.com/v1/methods", {method: "GET", headers})
+  //   .then((res) => res.json())
+  //   .then((data) =>setData(data))
+  //   .catch((error) => console.log(error));
+  //  },[]);
 
-//   const headers = {
-//     Authorization: 'Basic cnpwX3Rlc3RfU1p2a3hIOGlGT01mSmI6'
-//   }
-//   fetch("https://api.razorpay.com/v1/methods", {method: "GET", headers})
-//   .then((res) => res.json())
-//   .then((data) =>setData(data))
-//   .catch((error) => console.log(error));
-//  },[]);
+  useEffect(() => {
+    const fetchBankList = async () => {
+      try {
+        // Headers to be sent with the API request
+        const headers = {
+          Authorization: "Basic cnpwX3Rlc3RfU1p2a3hIOGlGT01mSmI6",
+          // Add other headers if needed
+        };
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const headers = {
-        'Authorization': 'Basic cnpwX3Rlc3RfU1p2a3hIOGlGT01mSmI6'
-      };
+        // Make API request to the fake bank list API
+        // Replace the URL with the actual API endpoint URL
+        const response = await axios.get("http://api.razorpay.com/v1/methods", {
+          headers: headers,
+        });
 
-      const response = await fetch('https://api.example.com/data', { headers });
-      if (!response.ok) {
-        throw new Error('Network response was not ok.');
+        setBanks(response.data.banks);
+      } catch (error) {
+        console.log(error);
       }
-      const data = await response.json();
-      console.log(data,'mnnnnnnnnnnnnnnnnnn')
-      setBanking(data);
-    } catch (error) {
-     
-    }
-  };
+    };
 
-  fetchData();
-}, []);
+    fetchBankList();
+  }, []);
 
- const listofBank =  banking.filter((banking) => banking.netbanking.toString() === (banking))
-
- 
- 
- return (
-   <div className="container">
-      <h1>Payment Page</h1>
-      { console.log(listofBank,'hhhhhhhhhhhhhhhh')}
+  return (
+    <div className="container">
       <form onSubmit={handleSubmit}>
+        <h1>Payment Page</h1>
         {/* payment method */}
         <div className="field-group">
           <label>
@@ -144,31 +143,78 @@ useEffect(() => {
               selectedPaymentMethod === "bankAccount" ? "show" : "hide"
             }`}
           >
-            <input
-              type="text"
-              name="ifscCode"
-              placeholder="IFSC Code"
-              value={bankAccount.ifscCode}
-              onChange={handleBankAccountChange}
-            />
-            <input
-              type="text"
-              name="accountHolderName"
-              placeholder="Account Holder Name"
-              value={bankAccount.accountHolderName}
-              onChange={handleBankAccountChange}
-            />
-            <input
-              type="text"
-              name="accountNumber"
-              placeholder="Account Number"
-              value={bankAccount.accountNumber}
-              onChange={handleBankAccountChange}
-            />
-           <select>
-          <option value="">Select a your Bank</option>
-         
-        </select>
+            <div>
+              <p>Select Your Bank</p>
+              <select name="doctype" id="doctype">
+                <option value="">Select your Bank</option>
+                <option value="AIRP">Airtel Payments Bank</option>
+                <option value="BARB_R">Bank of Baroda</option>
+                <option value="MAHB">Bank of Maharashtra</option>
+                <option value="CNRB">Canara Bank</option>
+                <option value="CSBK">Catholic Syrian Bank</option>
+                <option value="CBIN">Central Bank of India</option>
+                <option value="DCBL">DCB Bank</option>
+                <option value="DEUT">Deutsche Bank</option>
+                <option value="DLXB">Dhanlaxmi Bank</option>
+                <option value="ESFB">Equitas Small Finance Bank</option>
+                <option value="FSFB">Fincare Small Finance Bank</option>
+                <option value="IBKL">IDBI</option>
+                <option value="IDFB">IDFC FIRST Bank</option>
+                <option value="IDIB">Indian Bank</option>
+                <option value="IOBA">Indian Overseas Bank</option>
+                <option value="INDB">Indusind Bank</option>
+                <option value="JAKA">Jammu and Kashmir Bank</option>
+                <option value="JSFB">Jana Small Finance Bank</option>
+                <option value="KARB">Karnataka Bank</option>
+                <option value="KVBL">Karur Vysya Bank</option>
+                <option value="KKBK">Kotak Mahindra Bank</option>
+                <option value="LVB">Lakshmi Vilas Bank</option>
+                <option value="NSPB">NSDL Payments Bank</option>
+                <option value="PSIB">Punjab & Sind Bank</option>
+                <option value="PUNB_R">Punjab National Bank</option>
+                <option value="RATN">RBL Bank</option>
+                <option value="SVCB">SVC Co-Operative Bank Ltd</option>
+                <option value="SRCB">Saraswat Co-operative Bank</option>
+                <option value="SIBL">South Indian Bank</option>
+                <option value="SBI">State Bank of India</option>
+                <option value="TMBL">Tamilnad Mercantile Bank</option>
+                <option value="UCBA">UCO Bank</option>
+                <option value="UJVN">Ujjivan Small Finance Bank</option>
+                <option value="UBI">Union Bank of India</option>
+                <option value="VJB">Vijay Bank</option>
+                <option value="YB">Yes Bank</option>
+              </select>
+            </div>
+            <div>
+              <p>Enter Your AccountNumber</p>
+              <input
+                type="text"
+                name="accountNumber"
+                placeholder="Account Number"
+                value={bankAccount.accountNumber}
+                onChange={handleBankAccountChange}
+              />
+            </div>
+            <div>
+              <p>Enter Your AccountHolder Name</p>
+              <input
+                type="text"
+                name="accountHolderName"
+                placeholder="Account Holder Name"
+                value={bankAccount.accountHolderName}
+                onChange={handleBankAccountChange}
+              />
+            </div>
+            <div>
+              <p>Enter Your IFSC CODE</p>
+              <input
+                type="text"
+                name="ifscCode"
+                placeholder="IFSC Code"
+                value={bankAccount.ifscCode}
+                onChange={handleBankAccountChange}
+              />
+            </div>
           </div>
         )}
 
@@ -178,6 +224,10 @@ useEffect(() => {
               selectedPaymentMethod === "upi" ? "show" : "hide"
             }`}
           >
+            <div>
+              {" "}
+              <p>Enter Your UPI ID</p>
+            </div>
             <input
               type="text"
               placeholder="UPI ID"
@@ -193,6 +243,10 @@ useEffect(() => {
               selectedPaymentMethod === "creditCard" ? "show" : "hide"
             }`}
           >
+            <div>
+              <p>Enter Your Card Number</p>
+            </div>
+            <img src={Creditcard} height="25" alt="Preview" />
             <input
               type="text"
               name="cardNumber"
@@ -200,6 +254,12 @@ useEffect(() => {
               value={creditCard.cardNumber}
               onChange={handleCreditCardChange}
             />
+            <div>
+              <p>Enter Your Card Expiry Date</p>
+            </div>
+            <div>
+ 
+            <img src={expiredDate} alt="Preview" />
             <input
               type="text"
               name="expiryDate"
@@ -207,6 +267,12 @@ useEffect(() => {
               value={creditCard.expiryDate}
               onChange={handleCreditCardChange}
             />
+            </div>
+            <div>
+              <p>Enter Your Card CVV</p>
+            </div>
+            <img src={Cvv} height="25" alt="Preview" />
+
             <input
               type="text"
               name="cvv"
