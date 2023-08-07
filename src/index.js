648e552d58ed9878b06835dcdc6678bae584dbc5
laviManoj <lavigeremanoj@gmail.com>
2023-08-07 18:04:14 +0530

    Added Styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import $ from 'jquery';
+import Popper from 'popper.js';
+
 
 import App from "./App";
 import Payment from "./Payment";
