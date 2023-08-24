456c88dfc778c144281fcdaf99bf0e161694a07d
laviManoj <lavigeremanoj@gmail.com>
2023-08-24 17:45:24 +0530

    Added Bootstrap code in payment method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import $ from 'jquery';
-import Popper from 'popper.js';
+;
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 
 import App from "./App";
